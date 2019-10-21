27fa9bb23da56a836168041f6bcf0253b8e67150
H-Lala <56549842+H-Lala@users.noreply.github.com>
2019-10-21 16:20:55 +0400

    Task at class

---
+++ src/RandomNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Random R =new Random();
+        int length=20;
+        int [] arr = new int [length];
+        for (int i = 0; i <length ; i++) {
+            arr[i] = R.nextInt(10);
+        }
+        for (int i = 0; i <length ; i++) {
+            if(arr[i]%2==0){
+                System.out.println("fizz");
+            }
+             else if (arr[i]%3==0){
+                System.out.println("buzz");
+            }
+             else if(arr[i]%2==0 && arr[i]%3==0){
+                System.out.println("fizzbuzz");
+            }
+             else {
+                System.out.println(" ");
+            }
+
+        }
+    }
+}
