7b772e45a5bf1989e53f541e3b93df106c621573
H-Lala <56549842+H-Lala@users.noreply.github.com>
2019-10-23 16:37:48 +0400

    ArrayList task

---
+++ src/List.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class List {
+    public static void main(String[] args) {
+        Random R = new Random();
+       ArrayList <Integer> arr = new ArrayList<Integer>();
+        ArrayList <Integer> arrodd = new ArrayList<Integer>();
+        ArrayList <Integer> areven = new ArrayList<Integer>();
+        for (int i = 0; i <100 ; i++) {
+            arr.add(R.nextInt(100));
+        }
+        for (Integer item:arr) {
+            if (item % 2 == 0) {
+                areven.add(item);
+            } else {
+                arrodd.add(item);
+            }
+        }
+            System.out.println("Random numbers");
+            arr.forEach(System.out::println);
+
+            System.out.println("Odd numbers");
+            arrodd.forEach(System.out::println);
+
+            System.out.println("Even numbers");
+            areven.forEach(System.out::println);
+
+
+
+
+        }
+
+    }
+
+
