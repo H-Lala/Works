4713ecf7e2f4330e1d5963a20c826806f362a3c9
H-Lala <56549842+H-Lala@users.noreply.github.com>
2019-10-25 18:22:32 +0400

    HW

--- src/classwork/taskclass/List.java
+++ src/classwork/taskclass/List.java
@@ -1,3 +1,5 @@
+package classwork.taskclass;
+
 import java.util.ArrayList;
 import java.util.Random;
 
--- src/classwork/taskclass/RandomNumbers.java
+++ src/classwork/taskclass/RandomNumbers.java
@@ -1,3 +1,5 @@
+package classwork.taskclass;
+
 import java.util.Random;
 public class RandomNumbers {
     public static void main(String[] args) {
